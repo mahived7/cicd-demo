99f1b8e16d761b29459372091370c06a22df4007
Vedant Itkikar <msdsrtved0@gmail.com>
2026-03-21 03:07:22 +0530

    fresh start - cicd pipeline

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello CI/CD!');
+});
+
+if (require.main === module) {
+  app.listen(3000, () => console.log('Running on port 3000'));
+}
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,8 @@
+﻿const request = require('supertest');
+const app = require('./app');
+
+test('GET / returns 200', async () => {
+  const res = await request(app).get('/');
+  expect(res.status).toBe(200);
+  expect(res.text).toBe('Hello CI/CD!');
+});
